dfa6cc027813bf1c10eea1df74f4e9d252371b61
zeenkeen <zeenalam5@gmail.com>
2024-05-24 02:33:20 +0530

    i can't describe each work done

--- chaiaurdjango/chai/urls.py
+++ chaiaurdjango/chai/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
 from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name = 'home'),
-    path('about/', views.about, name = 'about'),
-    path('contact/', views.contact, name= 'contact'),
-    path('chai/',include('chai.urls') ),
+    
+    path('', views.all_chai, name = 'all_chai'),
+    path('order/',views.order, name = 'order'),
+
 
 ]
--- chaiaurdjango/chai/views.py
+++ chaiaurdjango/chai/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def all_chai(request):
+    return render(request, 'chai/all_chai.html')
+
+def order(request):
+    return render(request, 'chai/order.html')
