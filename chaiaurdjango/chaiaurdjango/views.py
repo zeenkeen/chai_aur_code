48b0d6c15bf7d3c923f4318cf2c11380afad1ae9
zeenkeen <zeenalam5@gmail.com>
2024-05-24 01:11:00 +0530

    configured settings for static files

--- chaiaurdjango/chaiaurdjango/views.py
+++ chaiaurdjango/chaiaurdjango/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     #return HttpResponse("hello world. This is chai aur django home page")
-    return render(request,'index.html')
+    return render(request,'website/index.html')
 def about(request):
     return HttpResponse("hello world. This is chai aur django about page")
 
